28667e7864a20f31655bc0d90128b803f793920f
David Luevano Alvarado <david@luevano.xyz>
2024-01-24 03:44:46 -0600

    feat: add logBacklog, avoids losing previous logs

--- log.go
+++ log.go
@@ -1,24 +1,32 @@
 package mangoprovider
 
-import "github.com/luevano/libmangal"
+import (
+	"fmt"
 
-var logger *libmangal.Logger
+	"github.com/luevano/libmangal"
+)
 
-// Log calls (*libmangal.Logger).Log() which is set by libmangal via Provider.SetLogger.
+var (
+	logBacklog []string
+	logger     *libmangal.Logger
+)
+
+// Log calls (*libmangal.Logger).Log(msg) when set, else it appends the message to the backlog.
+// Once the logger is set, it logs all backlog on the next log.
 //
 // It is the logger created by libmangal and used internally. Mangal also plugs into the logger (via (*libmangal.Client).Logger())
 // and uses it for info display.
-// 
-// It can also be set anywhere else, but usually it's something libmangal manages and since the Provider implementation
-// handles the "setup" we can intercept it and use externally (like here in mangoprovider).
-//
-// The logger is set once the provider is loaded, meaning that it will not be available for scraper/scraper.go for example,
-// but it will for scraper/search_mangas.go as by that point the provider has been loaded.
-//
-// TODO: provider mangal zerologger and use it when the *libmangal.Logger isn't available?
 func Log(msg string) {
-	// In case it is called before it is set
-	if logger != nil {
+	switch {
+	case logger != nil:
+		if len(logBacklog) != 0 {
+			for _, msg := range logBacklog {
+				logger.Log(fmt.Sprintf("[logBacklog] %s", msg))
+			}
+			logBacklog = nil
+		}
 		logger.Log(msg)
+	default:
+		logBacklog = append(logBacklog, msg)
 	}
 }
