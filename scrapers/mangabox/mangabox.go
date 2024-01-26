ebc301899734dd245a6de96efc57cf7051c8d67c
David Luevano Alvarado <david@luevano.xyz>
2024-01-26 16:07:13 -0600

    fix: mangabox has now better defaults

--- scrapers/mangabox/mangabox.go
+++ scrapers/mangabox/mangabox.go
@@ -14,16 +14,16 @@ import (
 // TODO: add manga search pager?
 
 // Mangabox is a generic type of website used by Manganato, Manganelo, Mangakakalot, etc.
-func Mangabox(name, baseUrl, dateLayout string) *scraper.Configuration {
+func Mangabox(name, baseUrl, searchPath, dateLayout string) *scraper.Configuration {
 	return &scraper.Configuration{
 		Name:            name,
 		Delay:           50 * time.Millisecond,
 		ReverseChapters: true,
 		BaseURL:         baseUrl,
 		GenerateSearchURL: func(baseUrl string, query string) (string, error) {
-			// path is /search/
+			query = strings.ReplaceAll(query, " ", "_")
 			u, _ := url.Parse(baseUrl)
-			u.Path = fmt.Sprintf("/search/%s", query)
+			u.Path = fmt.Sprintf(searchPath, query)
 
 			return u.String(), nil
 		},
@@ -39,7 +39,7 @@ func Mangabox(name, baseUrl, dateLayout string) *scraper.Configuration {
 				return selection.Find("a.item-img > img").AttrOr("src", "")
 			},
 			ID: func(_url string) string {
-				return strings.Split(_url, "/")[4]
+				return strings.Split(_url, "/")[3]
 			},
 		},
 		// TODO: Parse "Vol. #"?
@@ -61,7 +61,7 @@ func Mangabox(name, baseUrl, dateLayout string) *scraper.Configuration {
 				return title
 			},
 			ID: func(_url string) string {
-				return strings.Split(_url, "/")[4]
+				return strings.Join(strings.Split(_url, "/")[3:], "/")
 			},
 			URL: func(selection *goquery.Selection) string {
 				return selection.Find("a").AttrOr("href", "")
@@ -86,7 +86,7 @@ func Mangabox(name, baseUrl, dateLayout string) *scraper.Configuration {
 		PageExtractor: &scraper.PageExtractor{
 			Selector: "div#vungdoc img, div.container-chapter-reader img",
 			URL: func(selection *goquery.Selection) string {
-				return selection.AttrOr("data-src", "")
+				return selection.AttrOr("src", "")
 			},
 		},
 	}
--- scrapers/mangabox/manganato.go
+++ scrapers/mangabox/manganato.go
@@ -1,10 +1,6 @@
 package mangabox
 
 import (
-	"fmt"
-	"net/url"
-	"strings"
-
 	"github.com/luevano/libmangal"
 	mango "github.com/luevano/mangoprovider"
 	"github.com/luevano/mangoprovider/scraper"
@@ -13,7 +9,7 @@ import (
 var ManganatoInfo = libmangal.ProviderInfo{
 	ID:          mango.BundleID + "-manganato",
 	Name:        "Manganato",
-	Version:     "0.1.0",
+	Version:     "0.2.0",
 	Description: "Manganato scraper",
 	Website:     "https://manganato.com/",
 }
@@ -21,23 +17,9 @@ var ManganatoInfo = libmangal.ProviderInfo{
 var ManganatoConfig = manganato()
 
 func manganato() *scraper.Configuration {
-	m := Mangabox(ManganatoInfo.ID, ManganatoInfo.Website, "Jan 02,06")
+	m := Mangabox(ManganatoInfo.ID, ManganatoInfo.Website, "/search/story/%s", "Jan 02,06")
 
-	m.GenerateSearchURL = func(baseUrl, query string) (string, error) {
-		// path is /search/story/
-		// No longer required? the baseurl works just fine
-		// template := "https://chapmanganato.com/" + baseUrl + "/search/story/%s"
-		// return fmt.Sprintf(template, query), nil
-		query = strings.ReplaceAll(query, " ", "_")
-		u, _ := url.Parse(baseUrl)
-		u.Path = fmt.Sprintf("/search/story/%s", query)
-
-		return u.String(), nil
-	}
 	m.MangaExtractor.Selector = "div.search-story-item"
-	m.MangaExtractor.ID = func(_url string) string {
-		return strings.Split(_url, "/")[3]
-	}
 
 	return m
 }
